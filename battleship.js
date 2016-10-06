9e7511cc00d2ec5d0d0331994624a25ee6d7efcb
jessmccay <jessmccay@gmail.com>
2016-10-05 17:02:06 -0700

    Updated Battleship game

--- battleship.js
+++ battleship.js
@@ -2,74 +2,113 @@
 //signature: array, for loop, functions
 //example: user clicks square-- square turns red if target hit, yellow if missed.
 var board = [
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  []
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 var SHIP = 1;
+var HIT = 0;
+var torpedosUsed = 0
 
 $(document).ready(function() {
-  html="";
 
-    var box = 0;          //counter for making board spaces
-    var torpedosUsed = 0
-    for(row = 0; row < 10; row ++){      // make rows
+  makeBoard();
 
-      html= html+ "<tr>";    //when called adds row each time
+  makeShips();
 
-      for(data = 0; data < 10; data++){
-        html=html + '<td id="' + box + '"> </td>';     ///makes 10 spaces for each row
-        box++;
+  // TODO: user clicks on a square
+  // TODO: square changes color based on presence/absence of ship
+
+
+  $("td").on("click", function() {   // if user clicks board space function runs
+    // console.log("\n")
+
+
+
+     // if we find a ship
+     var clickedOn = $(this).attr("id"); // 56
+
+     var clickedOnRow = clickedOn.charAt(0); // 5
+     var clickedOnCol = clickedOn.charAt(1); // 6
+
+     console.log("clicked on :" + clickedOn);
+     console.log("clicked on row:" + clickedOnRow);
+     console.log("clicked on column:" + clickedOnCol);
+
+    // check for ship
+    if (HIT <= 5){
+      if (board[clickedOnRow][clickedOnCol] == 1) {
+        // show ship
+        $(this).text("🚢");
+        HIT++;
+        $("h3").text("HIT " + HIT);
+      } else {
+        // or show boom
+        $(this).addClass("boom")  // adding class to board space that user clicked
       }
 
-      html= html + "</tr>"   //ends row
-    }
-    $("table").append(html); //prints out board to user
-
-    // pieces are placed
-    shipPlace();
-    console.log(board);
-
-    // TODO: user clicks on a square
-    // TODO: square changes color based on presence/absence of ship
-
-
-    $("td").on("click", function() {   // if user clicks board space function runs
-      // console.log("\n")
-
-       $(this).addClass("boom")  // adding class to board space that user clicked
-       $(this).off();    //cannot re bomb a used board space
-       torpedosUsed++;       //counting torpedos that user has used
-       console.log(torpedosUsed + " torpedos fired");
+      $(this).off();    //cannot re bomb a used board space
+      torpedosUsed++;       //counting torpedos that user has used
+      console.log(torpedosUsed + " torpedos fired");
       $("h2").text(torpedosUsed + " torpedos fired");
 
-
-    });
-
-
-
+    };
+  });
 });
+  // if (shipChecker()) {
+  //   $("board[]").addClass("shipIsHere");
+  // }
 
-function shipPlace(){      //To create and insert shit to our board randomly
+
+// Add five random ships to our board
+function makeShips(){
   var shipsUsed = 0
   var col;
   var row;
+
   while(shipsUsed < 5) {        //run this code to get random number for row and col until ships are placed at different location for 5 times
-    col = Math.floor(Math.random() * 10);
-    row = Math.floor(Math.random() * 10);
+    col = Math.floor(Math.random() * 10);   // randomly generate col location within 0 - 10
+    row = Math.floor(Math.random() * 10);    // randomly generate row location within 0 - 10
     if (board[col][row] != SHIP) {            // to check if randomly selected location already have ship
       board[col][row] = SHIP                 //placing ship at randomly chosen location
-      shipsUsed++;
+      shipsUsed++;                            // add counter each time
     };
 
   }
 };
 
+// Make gameboard
+function makeBoard() {
+  html="";
+
+  for(row = 0; row < 10; row ++){      // make rows
+
+    html= html+ "<tr>";    //when called adds row each time
+
+    for(column = 0; column < 10; column++){
+      html=html + '<td id="' + row + column + '"> </td>';     ///makes 10 spaces for each row
+    }
+
+    html= html + "</tr>"   //ends row
+  }
+  $("table").append(html); //prints out board to user
+
+};
 // TODO: check for presence/absence of ship in a square, use this function up in controller (kind of like didWin in tic tac toe)
+// function shipChecker() {
+//   var col;
+//   var row;
+//   if (board[col][row] === 1) {
+//     return board[col][row];
+//     console.log(board[col][row])
+//
+//
+//   };
+// };
